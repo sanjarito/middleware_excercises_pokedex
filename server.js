7870ff231b0d32ec4775a648e477fcdcf416d121
Santiago Casar <santiago@nowhereprod.com>
2019-07-29 10:59:38 -0400

    Asda

--- server.js
+++ server.js
@@ -51,15 +51,7 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon',handleGetPokemon)
 
-app.use((error, req, res, next) => {
-  let response
-  if (process.env.NODE_ENV === 'production') {
-    response = { error: { message: 'server error' }}
-  } else {
-    response = { error }
-  }
-  res.status(500).json(response)
-})
+
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
